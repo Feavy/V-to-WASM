3d90ff7701604ec25bb5192dab09ec6aa0381578
Feavy <quentin071100@live.fr>
2021-05-24 01:09:20 +0200

    Added wasmer

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 v -os linux main.v -o main.c
-if not exist "dist" mkdir dist
-emcc -std=gnu11 -w -D__linux__ -s -lm -o dist/main.js placeholders.c main.c
+if not exist "static" mkdir static
+clang -w -Ofast -D__linux__ -target wasm32-unknown-wasi --sysroot C:\Users\Feavy\AppData\Local\WASI\share\wasi-sysroot -D_WASI_EMULATED_SIGNAL -lwasi-emulated-signal -Iinclude -Wl,--allow-undefined -o static/main.wasm main.c placeholders.c
