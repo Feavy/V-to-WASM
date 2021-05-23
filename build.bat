c1f2f4f86728f7ba2cb333ff35bf88b9373e8282
Feavy <quentin071100@live.fr>
2021-05-23 22:21:04 +0200

    First commit

---
+++ build.bat
@@ -0,0 +1,3 @@
+v -os linux main.v -o main.c
+if not exist "dist" mkdir dist
+emcc -std=gnu11 -w -D__linux__ -s -lm -o dist/main.js placeholders.c main.c
---
+++ main.v
@@ -0,0 +1,6 @@
+import time
+
+fn main() {
+	println("Hello world!")
+	println(time.now())
+}
